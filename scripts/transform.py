68cfe29127fc0727692c7120f8c3a4dbcf2b066b
hgera000 <hugo.lonhro@gmail.com>
2015-11-09 21:29:56 -0500

    Trying all the same pixel colour for non-yellow.

--- scripts/transform.py
+++ scripts/transform.py
@@ -21,20 +21,21 @@ def img_to_greyscale(filename):
     img = np.array(img) #dimensions 40,000 by 3
     #here convert to HSV scale
     img_hsv = [colorsys.rgb_to_hsv(*x/255) for x in img]
-    #set pixels not in yellow range to grey scale
-    img_hsv = [(x[0], 0, x[2]) if (x[0]*360 < 30 or x[0]*360 > 80) else (x[0], x[1], x[2]) for x in img_hsv]
+    #set pixels not in yellow range to black.
+    img_hsv = [(0, 0, 0) if (x[0]*360 < 40 or x[0]*360 > 79) else (x[0], x[1], x[2]) for x in img_hsv]
     img_rgb = [colorsys.hsv_to_rgb(*x) for x in img_hsv] #re-convert to rgb
     img_rgb = [(int(x[0]*255),int(x[1]*255),int(x[2]*255)) for x in img_rgb]
     
     im = Image.new("RGB", (200,200))
     im.putdata(img_rgb)
-    im.save("/home/hgera000/duality/metis-challenge/modified/%s.jpeg" % filename.split('/')[-1].split('.')[0] )
+    im.save("/home/hgera000/duality/metis-challenge/modified2/%s.jpeg" % filename.split('/')[-1].split('.')[0] )
 
     return 1
 
 
 img_dir = '/home/hgera000/duality/metis-challenge/beetrain/'
-images = [img_dir+ f for f in sorted(os.listdir(img_dir),key=lambda x: int(x.split('.')[0]))]
+sort_dir = sorted(os.listdir(img_dir),key=lambda x: int(x.split('.')[0]))[0:2000]
+images = [img_dir+ f for f in sort_dir]
 for image in images:
     img = img_to_greyscale(image)
 
