574992819d81ad0eb7ad547c6a0c2366ace224b5
hgera000 <hugo.lonhro@gmail.com>
2015-11-30 18:55:51 -0500

    Model to be used.

--- scripts/log.sh
+++ scripts/log.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 cd ~/duality/metis-challenge/scripts/
-python make_features.py &&  ~/local/R-3.0.2/bin/Rscript model.R
+python transform.py
+python make_features.py
+~/local/R-3.0.2/bin/Rscript model.R
 
--- scripts/model.R
+++ scripts/model.R
@@ -6,34 +6,68 @@ library(pROC)
 
 setwd('~/duality/metis-challenge/data')
 
+rm(list=ls())
+
 labels = fread('beetrainLabels.csv')
 labels[,genus:=ifelse(genus==1,'bumble',ifelse(genus==0,'honey',NA))]
 labels[,genus:=factor(genus)]
 
-train = fread('pca.train.csv')
-ids = fread('names.train.csv')
+#Load PCA features.
+train = fread('pca.train.csv',select=c("V1","V2","V3","V4","V5"))
+ids = fread('names.train.csv',select=c("V1","V2","V3","V4","V5"))
 setnames(ids,"id")
 train = cbind(ids,train)
 train = merge(labels,train,by="id")
 
-test = fread('pca.test.csv')
-ids = fread('names.test.csv')
+train2 = fread('pca.grey.train.csv',select=c("V1","V2","V3","V4","V5"))
+ids2 = fread('names.grey.train.csv',select=c("V1","V2","V3","V4","V5"))
+setnames(ids2,"id")
+train2 = cbind(ids2,train2)
+train = merge(train,train2,by="id")
+
+test = fread('pca.test.csv',select=c("V1","V2","V3","V4","V5"))
+ids = fread('names.test.csv',select=c("V1","V2","V3","V4","V5"))
 setnames(ids,"id")
 test = cbind(ids,test)
 test = merge(labels,test,by="id")
 
+test2 = fread('pca.grey.test.csv',select=c("V1","V2","V3","V4","V5"))
+ids2 = fread('names.grey.test.csv',select=c("V1","V2","V3","V4","V5"))
+setnames(ids2,"id")
+test2 = cbind(ids2,test2)
+test = merge(test,test2,by="id")
+
+
+#Load orange/yellow pixel count features. 
+orange = fread('count_orange.csv')
+setnames(orange,"orange")
+yellow = fread('count_yellow.csv')
+setnames(yellow,"yellow")
+ids = fread('names.transform.csv')
+setnames(ids,"id")
+other = cbind(ids,orange,yellow)
+other[,share:=orange/(yellow+1)]
+
+
+#merge all features together. 
+train = merge(train,other,by="id")
+test = merge(test,other,by="id")
+test_ids = test[,id]
+
 #regression formula - drop id and y variable. 
-#form = as.formula(paste("~0",paste(colnames(train)[-c(1,2)],collapse="+"),sep="+"))
-rhs.train = model.matrix(genus~0+. -id,data=train)
-rhs.test = model.matrix(genus~0+. -id,data=test)
-#below to include interactions
-## rhs.train = model.matrix(genus~0+.*. -id,data=train)
-## rhs.test = model.matrix(genus~0+.*. -id,data=test)
+####form = as.formula(paste("~0",paste(colnames(train)[-c(1,2)],collapse="+"),sep="+"))
+#rhs.train = model.matrix(genus~0+. -id,data=train)
+#rhs.test = model.matrix(genus~0+. -id,data=test)
+##below to include interactions
+train[,id:=NULL]
+test[,id:=NULL]
+rhs.train = model.matrix(genus~0+.*. ,data=train)
+rhs.test = model.matrix(genus~0+.*. ,data=test)
 
 
 set.seed(345)
 
-trControl=trainControl(method='cv',number=5,verboseIter=TRUE,classProbs=TRUE,summaryFunction=twoClassSummary)
+trControl=trainControl(method='cv',number=10,verboseIter=TRUE,classProbs=TRUE,summaryFunction=twoClassSummary)
 
 
 #-----------
@@ -53,6 +87,7 @@ fit.rf<-train(rhs.train,train$genus,
            #do.trace=TRUE)
 print(fit.rf)
 #print(summary(fit.rf))
+varImp(fit.rf)
 
 print(fit.rf$resample$ROC)
 print(mean(fit.rf$resample$ROC))
@@ -64,28 +99,46 @@ print(roc(test$genus,outpred.rf$bumble)$auc)
 #-----------
 #LASSO
 #-----------
-lambdas = glmnet(rhs.train,y=train$genus,family='binomial',standardize=T,intercept=T)$lambda
+## #via caret
+## lambdas = glmnet(rhs.train,y=train$genus,family='binomial')$lambda
 
-tuneGrid = expand.grid(alpha=1,lambda=lambdas)
-#tuneGrid = expand.grid(alpha=1,lambda=lammin)
+## tuneGrid = expand.grid(alpha=1,lambda=lambdas)
+## #tuneGrid = expand.grid(alpha=1,lambda=0.00156)
 
-fit.lasso = train(rhs.train,train$genus,
-               method="glmnet",
-               family="binomial",
-               trControl=trControl,
-               metric="ROC", #ROC #Accuracy
-               tuneGrid=tuneGrid,
-               preProc = c("center", "scale")
-               )
+## fit.lasso = train(rhs.train,train$genus,
+##                method="glmnet",
+##                family="binomial",
+##                trControl=trControl,
+##                metric="ROC", #ROC #Accuracy
+##                tuneGrid=tuneGrid,
+##                preProc = c("center", "scale")
+##                )
 
-#print(fit.lasso)
-#print(summary(fit.lasso))
-print(fit.lasso$resample$ROC)
-print(mean(fit.lasso$resample$ROC))
+## #print(fit.lasso)
+## #print(summary(fit.lasso))
+## varImp(fit.lasso)
+## print(fit.lasso$resample$ROC)
+## print(mean(fit.lasso$resample$ROC))
 
-outpred.lasso = predict.train(fit.lasso,type="prob",newdata=rhs.test)
+## lam = fit.lasso$finalModel$lambdaOpt
+## #lam = 0.00156
+## coefs = coef(fit.lasso$finalModel,lam)
+## test@Dimnames[[1]][test@i]
+
+## outpred.lasso = predict(fit.lasso,type="prob",newdata=rhs.test)
+
+#via glmnet directly - allows to use lambda1se. 
+fit.lasso = cv.glmnet(rhs.train,train$genus,nfolds=10,family='binomial',type.measure="auc")
+
+#Save two key 'lambdas' governing degree of regularization. 1se will be used for forecasting.
+lam1se = fit.lasso$lambda.1se
+lammin = fit.lasso$lambda.min
+
+outpred.lasso = data.table(honey=predict(fit.lasso,newx=rhs.test,s=lam1se,type='response')[,1])
+outpred.lasso[,bumble := 1-honey]
 
 print(roc(test$genus,outpred.lasso$bumble)$auc)
+
 
 
 
@@ -93,7 +146,7 @@ print(roc(test$genus,outpred.lasso$bumble)$auc)
 print(roc(test$genus,0.5*(outpred.rf$bumble)+0.5*(outpred.lasso$bumble))$auc)
 
 #finalforecast
-output = data.frame(id=test$id,genus=0.5*(outpred.rf$bumble)+0.5*(outpred.lasso$bumble))
+output = data.frame(id=test_ids,genus=0.5*(outpred.rf$bumble)+0.5*(outpred.lasso$bumble))
 write.table(output,file="output.csv",sep=",",row.names=FALSE)
 
 
