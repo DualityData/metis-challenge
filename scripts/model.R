5ab58cda440ebe6777dbaa107f1c7f5fc78f0556
hgera000 <hugo.lonhro@gmail.com>
2015-12-03 08:36:15 -0500

    forcs here.

--- scripts/model.R
+++ scripts/model.R
@@ -14,13 +14,13 @@ labels[,genus:=factor(genus)]
 
 #Load PCA features.
 train = fread('pca.train.csv',select=c("V1","V2","V3","V4","V5"))
-ids = fread('names.train.csv',select=c("V1","V2","V3","V4","V5"))
+ids = fread('names.train.csv',select=c("V1","V2","V3","V4","V5"))[1:3000]
 setnames(ids,"id")
 train = cbind(ids,train)
 train = merge(labels,train,by="id")
 
 train2 = fread('pca.grey.train.csv',select=c("V1","V2","V3","V4","V5"))
-ids2 = fread('names.grey.train.csv',select=c("V1","V2","V3","V4","V5"))
+ids2 = fread('names.grey.train.csv',select=c("V1","V2","V3","V4","V5"))[1:3000]
 setnames(ids2,"id")
 train2 = cbind(ids2,train2)
 train = merge(train,train2,by="id")
@@ -29,7 +29,7 @@ test = fread('pca.test.csv',select=c("V1","V2","V3","V4","V5"))
 ids = fread('names.test.csv',select=c("V1","V2","V3","V4","V5"))
 setnames(ids,"id")
 test = cbind(ids,test)
-test = merge(labels,test,by="id")
+#test = merge(labels,test,by="id")
 
 test2 = fread('pca.grey.test.csv',select=c("V1","V2","V3","V4","V5"))
 ids2 = fread('names.grey.test.csv',select=c("V1","V2","V3","V4","V5"))
@@ -48,9 +48,22 @@ setnames(ids,"id")
 other = cbind(ids,orange,yellow)
 other[,share:=orange/(yellow+1)]
 
-
 #merge all features together. 
 train = merge(train,other,by="id")
+
+
+#Load orange/yellow pixel count features. 
+orange = fread('count_orange.test.csv')
+setnames(orange,"orange")
+yellow = fread('count_yellow.test.csv')
+setnames(yellow,"yellow")
+ids = fread('names.transform.test.csv')
+setnames(ids,"id")
+other = cbind(ids,orange,yellow)
+other[,share:=orange/(yellow+1)]
+
+#merge all features together. 
+
 test = merge(test,other,by="id")
 test_ids = test[,id]
 
@@ -62,7 +75,7 @@ test_ids = test[,id]
 train[,id:=NULL]
 test[,id:=NULL]
 rhs.train = model.matrix(genus~0+.*. ,data=train)
-rhs.test = model.matrix(genus~0+.*. ,data=test)
+rhs.test = model.matrix(~0+.*. ,data=test)
 
 
 set.seed(345)
@@ -139,14 +152,11 @@ outpred.lasso[,bumble := 1-honey]
 
 #print(roc(test$genus,outpred.lasso$bumble)$auc)
 
-
-
-
 #combination
 #print(roc(test$genus,0.5*(outpred.rf$bumble)+0.5*(outpred.lasso$bumble))$auc)
 
 #finalforecast
-output = data.frame(id=test_ids,genus=0.7*(outpred.rf$bumble)+0.3*(outpred.lasso$bumble))
+output = data.frame(id=test_ids,genus=0.75*(outpred.rf$bumble)+0.25*(outpred.lasso$bumble))
 write.table(output,file="output.csv",sep=",",row.names=FALSE)
 
 
